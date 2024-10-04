119762e1975daacdcedbf1f5ca899629440a265b
曙光 <5248283+lsg15829561090@user.noreply.gitee.com>
2024-10-04 21:22:40 +0800

    优化

--- src/app/components/TodoList.tsx
+++ src/app/components/TodoList.tsx
@@ -1,13 +1,18 @@
+// 导入TodoItem组件
 import TodoItem from "./TodoItem";
+// 导入Todo类型
 import { Todo } from "../types";
 
+// 定义TodoListProps接口，包含todos、deleteTodo、toggleTodo三个属性
 interface TodoListProps {
   todos: Array<Todo>;
   deleteTodo: (id: number) => void;
   toggleTodo: (id: number) => void;
 }
 
+// 定义TodoList组件，接收TodoListProps类型的props
 const TodoList = ({ todos, deleteTodo, toggleTodo }: TodoListProps) => {
+  // 返回一个ul元素，包含todos数组中的每个todo元素
   return (
     <ul>
       {todos.map((todo) => (
@@ -22,4 +27,5 @@ const TodoList = ({ todos, deleteTodo, toggleTodo }: TodoListProps) => {
   );
 };
 
+// 导出TodoList组件
 export default TodoList;
